73471f68f3e12886628a31b57d7231a97bef7c43
BenVizena <BenVizena@gmail.com>
2016-07-29 21:56:39 -0500

    Set up database for freerun ui options

--- app/src/main/java/com/example/android/dat_running_app/FreeRunChangeUI.java
+++ app/src/main/java/com/example/android/dat_running_app/FreeRunChangeUI.java
@@ -2,11 +2,19 @@ package com.example.android.dat_running_app;
 
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
+import static android.R.attr.button;
 import static com.example.android.dat_running_app.RunningScreenService.MY_ACTION;
 
 /**
@@ -14,6 +22,8 @@ import static com.example.android.dat_running_app.RunningScreenService.MY_ACTION
  */
 
 public class FreeRunChangeUI extends AppCompatActivity {
+    private FreeRunDBHelper frDB;
+    private Button commitChangesButton;
     private static boolean totalTime;
     private static boolean totalDistance;
 
@@ -22,6 +32,9 @@ public class FreeRunChangeUI extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_free_run_change_ui);
 
+        frDB = new FreeRunDBHelper(this);
+        commitChangesButton = (Button)findViewById(R.id.confirmFreeRunUIButton);
+        getSettings();
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -30,7 +43,7 @@ public class FreeRunChangeUI extends AppCompatActivity {
         int height = dm.heightPixels;
 
         getWindow().setLayout((int) (width * .8), (int) (height * .5));
-
+/*
         Thread t = new Thread() {
 
             @Override
@@ -53,22 +66,91 @@ public class FreeRunChangeUI extends AppCompatActivity {
         };
 
         t.start();
+*/
 
+    }
+/*
+    public void commitChanges(){
+        commitChangesButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Log.d("DB TAG","PRE INSERTED");
+                        boolean isInserted = frDB.addSettings(totalTime, totalDistance);
+                        if(isInserted)
+                            Log.d("DB TAG","INSERTED");
+                        else
+                            Log.d("DB TAG","NOT INSERTED");
+                    }
+                }
+        );
+    }
+ */
+    public void commitChanges(View view){
+        refreshTotalTime();
+        refreshTotalDistance();
+        boolean isInserted = frDB.addSettings(totalTime, totalDistance);
+        Log.d("THIS IS SENT",""+totalTime+" "+totalDistance+"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        if(isInserted)
+            Log.d("DB TAG","INSERTED");
+        else
+            Log.d("DB TAG","NOT INSERTED");
+        Intent intent = new Intent(this,FreeRunClickedMenu.class);
+        startActivity(intent);
 
+    }
+
+    public void getSettings(){
+        Cursor settings = frDB.getFRSettings();
+        CheckBox stt;
+        stt = (CheckBox)findViewById(R.id.showTimeCheckBox);
+        CheckBox std;
+        std = (CheckBox) findViewById(R.id.showDistanceCheckBox);
+
+        if (!settings.moveToFirst())
+            settings.moveToFirst();
+
+        if(settings.getCount()==0){
+            showMessage("ERROR","NOTHING FOUND");
+        }
+        else{
+            if((""+settings.getString(1)).equals("true")){
+                totalTime=true;
+                stt.toggle();
+            }
+            else if(stt.isChecked()){
+                totalTime=false;
+                stt.toggle();
+                Log.d("TOGGLE","TIME OFF");
+            }
+            if(settings.getString(2).equals("true")){
+                totalDistance=true;
+                std.toggle();
+            }
+            else if(std.isChecked()){
+                totalDistance=false;
+                std.toggle();
+                Log.d("TOGGLE","DISTANCE OFF");
+            }
+         //   showMessage("DATA","TOTALTIME:"+totalTime+" "+"TOTALDISTANCE: "+totalDistance);
+
+        }
     }
 
     private void refreshTotalTime(){
-        RadioButton stt;
-        stt = (ToggleableRadioButton)findViewById(R.id.showTimeRadioButton);
+        CheckBox stt;
+        stt = (CheckBox) findViewById(R.id.showTimeCheckBox);
         if(stt.isChecked())
             totalTime=true;
-        else
+        else{
             totalTime=false;
+        }
+
     }
 
     private void refreshTotalDistance(){
-        ToggleableRadioButton std;
-        std = (ToggleableRadioButton)findViewById(R.id.showDistanceRadioButton);
+        CheckBox std;
+        std = (CheckBox) findViewById(R.id.showDistanceCheckBox);
         if(std.isChecked())
             totalDistance=true;
         else
@@ -81,5 +163,14 @@ public class FreeRunChangeUI extends AppCompatActivity {
 
     public static boolean showTotalDistance(){
         return totalDistance;
+    }
+
+
+    public void showMessage(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
---
+++ app/src/main/java/com/example/android/dat_running_app/FreeRunDBHelper.java
@@ -0,0 +1,82 @@
+package com.example.android.dat_running_app;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import static android.R.attr.version;
+
+
+/**
+ * Created by Ben on 7/27/2016.
+ */
+
+public class FreeRunDBHelper extends SQLiteOpenHelper{
+
+    public static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "dat_running_app_db.db";
+    public static final String TABLE_NAME = "freerun_settings_table";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_SHOWTIME = "SHOW_TIME";
+    public static final String COLUMN_SHOWDISTANCE = "SHOW_DISTANCE";
+
+
+    public FreeRunDBHelper(Context context) {//, String name, SQLiteDatabase.CursorFactory factory, int version
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String query = "CREATE TABLE " + TABLE_NAME + "(" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_SHOWTIME + " TEXT, " +
+                COLUMN_SHOWDISTANCE + " TEXT " +
+                ")";
+        sqLiteDatabase.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public boolean addSettings(boolean showTotalTime, boolean showTotalDistance){
+        deleteRow();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_SHOWTIME,""+showTotalTime);
+        values.put(COLUMN_SHOWDISTANCE,""+showTotalDistance);
+        SQLiteDatabase db = getWritableDatabase();
+        long success = db.insert(TABLE_NAME,null,values);
+        db.close();
+
+        Log.d("degub", ""+success);
+
+        if(success == -1)
+            return false;
+        else
+            return true;
+
+    }
+
+    private boolean deleteRow(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        boolean result = db.delete(TABLE_NAME,null,null)>0;
+        db.close();
+        return result;
+    }
+
+    public Cursor getFRSettings(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM "+ TABLE_NAME;
+        Cursor result = db.rawQuery(query, null);
+        if (!result.moveToFirst())
+            result.moveToFirst();
+        Log.d("RESULTS",".getInt(0): "+result.getInt(0)+" .getString(1): "+result.getString(1)+" .getString(2): "+result.getString(2)+"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        db.close();
+        return result;
+    }
+}
